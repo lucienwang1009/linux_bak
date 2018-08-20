31ab3f1782bb46bde583f56e1d822911b7241561
Lucien Wang <lucienwang@qq.com>
2018-08-20 03:06:48 +0000

    refine

--- setup.sh
+++ setup.sh
@@ -1,27 +1,28 @@
+# install softwares
+sudo apt-get update
+sudo apt-get install -y zsh tmux sudo git
+
 # mkdir necessary dirs
-mkdir ~/bin ~/download ~/softwares ~/workspace ~/demo
+mkdir /home/$user_name/bin /home/$user_name/download /home/$user_name/softwares /home/$user_name/workspace /home/$user_name/demo
 
 # zsh install
-sudo apt-get update
-sudo apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp .zshrc ~/
+cp .zshrc /home/$user_name
 
 # tmux install
-sudo apt-get install -y tmux
-cp .tmux.conf ~/
-cp tmux.sh ~/bin
+cp .tmux.conf /home/$user_name/
+cp tmux.sh /home/$user_name/bin
 
 # vim install
-cp -r .vim_runtime ~/
-bash ~/.vim_runtime/install_awesome_vimerc.sh
+cp -r .vim_runtime /home/$user_name/
+bash /home/$user_name/.vim_runtime/install_awesome_vimrc.sh
 
 # pbcopy install
 bash pbcopy.sh
 
 # anaconda install
-wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh ~/download
-bash ~/download/Anaconda3-5.2.0-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh -P /home/$user_name/download/
+bash /home/$user_name/download/Anaconda3-5.2.0-Linux-x86_64.sh
 
 # generate ssh pub key
 ssh-keygen
